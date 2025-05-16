b6cf273371766dbf4f3169c5d07e7d74ee2247eb
Opera <159807320+OperavonderVollmer@users.noreply.github.com>
2025-05-17 03:39:47 +0800

    Update REST.py

--- REST/REST.py
+++ REST/REST.py
@@ -10,7 +10,7 @@ app = FastAPI()
 
 @app.get("/")
 def root():
-    return {"status": "OK"}
+    return {"status": "OK", "status_code": 200}
 
 @app.get("/forecast")
 def forecast(city: BaseModels.City = Depends(Current.get_current_city)):
@@ -18,8 +18,8 @@ def forecast(city: BaseModels.City = Depends(Current.get_current_city)):
     forecast_data = Forecast.get_forecast(city.city)
     return {"status": "OK", "status_code": 200, "default_city": city.city or "Unset" , "payload": forecast_data}
 
-# curl -X POST -H "Content-Type: application/json" -d "{\"city\": \"London\"}" "http://127.0.0.1:8000/forecast"
-@app.post("/forecast")
+
+@app.post("/forecast") # curl -X POST -H "Content-Type: application/json" -d "{\"city\": \"London\"}" "http://127.0.0.1:8000/forecast"
 def change_city(set_city: BaseModels.City):
     CITY = set_city.city
 
